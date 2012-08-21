79b8880088809e0bec1181ec1ad9de54fef86a38
marco <marcoartaviaq@yahoo.es>
2012-08-21 14:15:56 -0600

    Splash hecho

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
@@ -2,7 +2,9 @@ package com.fr4gus.android.oammblo.ui;
 
 import com.fr4gus.android.oammblo.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Shows App logo for few seconds.
@@ -11,11 +13,26 @@ import android.os.Bundle;
  * Created Mar 25, 2012
  */
 public class SplashActivity extends OammbloActivity {
+	
+	Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    	mHandler.postDelayed(new Runnable(){
+    		public void run(){
+    			Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+    			startActivity(intent);
+    		}
+    		
+    	}, 2000);
     }
 
 }
